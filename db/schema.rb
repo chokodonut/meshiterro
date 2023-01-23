f6f695de0c5f78c56e83718d308bf7041b4e7628
chokodonut <121842407+chokodonut@users.noreply.github.com>
2023-01-23 12:59:41 +0000

    9章途中

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_22_042532) do
+ActiveRecord::Schema.define(version: 2023_01_23_122244) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -45,6 +45,13 @@ ActiveRecord::Schema.define(version: 2023_01_22_042532) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "title"
     t.string "body"
+  end
+
+  create_table "post_images", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
